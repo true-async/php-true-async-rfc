e7049a471d111c5a0f4bbddde3ef41049871d9a4
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2025-03-17 20:42:37 +0200

    % refactoring BoundedScope

--- examples/Async/BoundedScope.php
+++ examples/Async/BoundedScope.php
@@ -10,8 +10,8 @@ final class BoundedScope extends Scope
      *
      */
     public function spawnAndBound(\Closure $coroutine, ...$args): Coroutine {}
-    public function withTimeout(int $milliseconds): void {}
-    public function boundedBy(Awaitable $constraint): void {}
+    public function defineTimeout(int $milliseconds): void {}
+    public function boundScope(Awaitable $constraint): void {}
     
     /**
      * Sets an error handler that is called when an exception is passed to the Scope from one of its child coroutines.
@@ -25,7 +25,7 @@ final class BoundedScope extends Scope
     public function setChildScopeExceptionHandler(callable $exceptionHandler): void {}
     
     /**
-     * Returns the bound information of the scope in string format.
+     * Returns the bound information of the scope in array format.
      */
     public function getBoundsInfo(): array {}
 }
